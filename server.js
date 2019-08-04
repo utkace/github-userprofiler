42ae71825eb05ed93e3fe952547a179c9427610c
utkarsh maharana <utkarshm27@hotmail.com>
2019-08-05 00:10:48 +0530

    redis memdb setup

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
 const express = require("express");
 const sqlite3 = require("sqlite3").verbose();
+const redis = require("redis");
+
+// create and connect redis client to local instance.
+const client = redis.createClient(
+  "redis://h:p72fc864113c1bec4e6ffeed160f944e171392e161e056414b88cdf3b80a1c499@ec2-54-162-141-93.compute-1.amazonaws.com:29479"
+);
+
+//connecting to redis
+client.on("connect", () => {
+  console.log("cache connected!");
+});
+
+client.on("error", err => {
+  console.log(err);
+});
 
 //Initializing database
 let db = new sqlite3.Database("./db/githubUsers.db", err => {
