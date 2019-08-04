0e3c41e05ae83dd52b4309367a2c39173a232f8d
utkarsh maharana <utkarshm27@hotmail.com>
2019-08-05 00:08:33 +0530

    setup sqlite db

--- server.js
+++ server.js
@@ -1,4 +1,13 @@
 const express = require("express");
+const sqlite3 = require("sqlite3").verbose();
+
+//Initializing database
+let db = new sqlite3.Database("./db/githubUsers.db", err => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log("Connected to the SQlite database.");
+});
 
 const app = express(); //setup express
 
